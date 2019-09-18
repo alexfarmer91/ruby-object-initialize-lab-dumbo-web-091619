b58d7dae4db993d86700fc17c37da1c3220f3d23
Alex Farmer <farmale10@gmail.com>
2019-09-18 12:54:45 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,6 +2,16 @@
 class Person 
   
   def initialize(name)
+    @breed = breed
+  end 
+  
+  def breed=(new_breed)
+    @breed = new_breed
+  end 
+  
+  def breed 
+    @breed 
+  end 
   
   
   
